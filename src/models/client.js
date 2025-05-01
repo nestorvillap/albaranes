78dd234aba9388cb60e102510cdae4c7555d1eb8
Nestor Villa <nestorvillap@gmail.com>
2025-05-01 20:54:09 +0200

    feat: add  cif and address on client model

--- src/models/client.js
+++ src/models/client.js
@@ -2,6 +2,8 @@ import { Schema, model } from 'mongoose'
 
 const clientSchema = new Schema({
   name: { type: String, required: true },
+  cif: { type: String },
+  address: { type: String },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
